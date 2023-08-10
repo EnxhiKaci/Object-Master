e9ebebd7a1f1938faf741c402f62ed5c08621eae
EnxhiKaci <141232646+EnxhiKaci@users.noreply.github.com>
2023-08-10 18:13:59 +0200

    Object-Master.js

--- Object-Master.js
+++ Object-Master.js
@@ -27,3 +27,23 @@ const pokémon = Object.freeze([
  const array = pokémon.filter (item=>item.id%3==0);
  console.log(array) 
 
+ const array1=pokemon.filter(item=>item.types.includes('fire'));
+ console.log(array1);
+ 
+ const array2=pokemon.filter(item=>item.types.length>1);
+ console.log(array2);
+ 
+ const array3=pokemon.map(item=>item.name);
+ console.log(array3);
+
+ const array4=pokemon.filter(item=>item.id>99).map(item=>item.name);
+ console.log(array4);
+
+ const array5=pokemon.filter(item=>item.types.includes('poison'));
+ console.log(array5);
+
+ const array6=pokemon.filter(item=>item.types.length===2&&item.types[1]===('flying'));
+ console.log(array6);
+
+ const array7=pokemon.filter(item=>item.types.includes('normal')).length;
+ console.log(array7);
